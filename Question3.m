b2f4532194fd5c9f910c2980c92a6c8cdf8f45bc
naudius <89517580+naudius@users.noreply.github.com>
2022-02-28 12:41:11 +0200

    Final report

--- Question3.m
+++ Question3.m
@@ -4,6 +4,7 @@
    ends = 70;
    n = (starts:1:ends)';
 %%Signals
+   g = figure;
    u = zeros(size(n));
    u(n>=0) = 1;
    x = u.*exp(1j*pi/10*n);
@@ -22,12 +23,12 @@
    end
    subplot(2,2,3),stem(n,real(y)),title('Real(Y[n])');
    subplot(2,2,4),stem(n,imag(y)),title('Img(Y[n])');
-
+   print(g, '-dpng', 'Question3.png'); 
  %%Calculating A and phi
    A = abs(y); %since original magnitude is 1, tested by abs(y./x) == abs(y)
    phi = angle(y./x);
  %% choosing large value of array to avoid transients
    A_num = A(50);
    phi_num = phi(50);
-   disp(phi_num);
-   disp(A_num);
+   disp("Magnitude of frequency response: "+A_num);
+   disp("Phase of frequency response: "+phi_num);
